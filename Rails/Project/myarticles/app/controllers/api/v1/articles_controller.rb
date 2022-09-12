909d1e25addb892eac545be3984e6265a2dd1156
Naman Dua <duanaman9@gmail.com>
2022-09-12 11:59:33 +0530

    Added function of create and destroy record

--- Rails/Project/myarticles/app/controllers/api/v1/articles_controller.rb
+++ Rails/Project/myarticles/app/controllers/api/v1/articles_controller.rb
@@ -10,6 +10,26 @@ module Api
                 article = Article.find(params[:id])
                 render json: {status: 'Success', message: 'Article loaded', data:article}, status: :ok
             end
+            def create
+                article = Article.new(article_params)
+
+                if article.save
+                    render json: {status: 'Success', message: 'Article Saved', data:article}, status: :ok
+                else
+                    render json: {status: 'Error', message: 'Article not saved', data:article.errors}, status: :unprocessable_entity
+                end
+            end
+
+            def destroy
+                article = Article.find(params[:id])
+                article.destroy
+                render json: {status: 'Success', message: 'Article destroyed', data:article}, status: :ok
+            end
+            private
+
+            def article_params
+                params.permit(:title, :body)
+            end
         end
     end
 end
