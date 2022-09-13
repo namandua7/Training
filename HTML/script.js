28f25d756a0e6674602aacc199ac088a23201797
Naman Dua <duanaman9@gmail.com>
2022-09-13 18:32:55 +0530

    Added a new file of html and js in which js code is practiced

---
+++ HTML/script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById('demo').innerHTML="I'm a paragraph";
+}
+
+function windowAlert() {
+    alert('You clicked blue button');
+}
+
+function findPower(num){
+    alert('Value of number to the power number is ' + num**num)
+}
